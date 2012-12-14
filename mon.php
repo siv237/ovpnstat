32669fc9b048f822ace812e94b9fcf9e5d50cbac
root <root@jdeoffice.local>
2012-12-14 22:51:53 +1100

    Убрана нормализация, теперь файл как есть

--- mon.php
+++ mon.php
@@ -1,10 +1,10 @@
 <?php
  $fileName= $_GET["fname"];
  $pathFile=exec("/usr/bin/find /var/spool/asterisk/monitor/ -name '$fileName'");
- $tmpFile="/var/www/tmp/$fileName";
+// $tmpFile="/var/www/tmp/$fileName";
 // echo $tmpFile;
- exec("sox --norm -v 10 $pathFile $tmpFile");
+// exec("sox --norm -v 10 $pathFile $tmpFile");
  header("Content-Disposition: attachment; filename=$fileName");
- readfile("$tmpFile"); 
+ readfile("$pathFile"); 
  exit(); 
 ?>
