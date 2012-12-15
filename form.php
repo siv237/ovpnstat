6385868270b5f70048e7c4bd2b91eceb60a91342
root <root@jdeoffice.local>
2012-12-15 18:29:45 +1100

    Добавлены фильтры поиска 2

---
+++ form.php
@@ -0,0 +1,12 @@
+<?php
+$to= $_GET["to"];
+
+//if (isset($_POST['submit'])) 
+system ("grep ".$to." list.csv |awk -F ';' '{print $2}'");
+echo "<br>Хочу, чтобы система соединила меня.<br>";
+echo "<form method='post' action='originate.php'>";
+echo "Мой : <input type='text' name='v1' value='101'><br>";
+echo "Кому: <input type='text' name='v2' value=".$to.">";
+echo "<br><input type='submit' name='submit' value='Соединить'></form>";
+
+?>
