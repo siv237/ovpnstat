4251fff01c0c1548b92e99c62322a887b8bfa66d
root <root@jde.dnsalias.net>
2012-12-29 21:28:29 +0400

    Добавлено распознование сотовых

--- formatnum.php
+++ formatnum.php
@@ -3,6 +3,32 @@ function FormatTelNum($tel)
 {
 $tel=substr($tel,-10);	
 $origtel=$tel;
+
+// Если телефон начинается с 9, значит обрабатываем как сотовый
+if ($tel[0] == 9)
+{
+$origtel=$tel;
+$base=fopen("rusotdef.csv","r");
+while (!feof($base)) 
+        {
+        $strm=explode(";",fgets($base));
+        if ($tel > $strm[0] and $tel < $strm[1])
+                {
+                $intrv=$strm[1]-$strm[0];
+                $tel=substr_replace($tel,"-",-2,0);
+                $tel=substr_replace($tel,"-",-5,0);
+                $tel=substr_replace($tel,") ",-9,0);
+                $tel=substr_replace($tel,"+7 (",-14,0);
+                echo "Сот. ".$tel." ".$strm[2].", ".$strm[3];
+                }       
+        }
+fclose($base);
+}
+
+// Если телефон не сотовый то обрабатываем как городской
+else
+{
+
 $base=fopen("rutelcode.csv","r");
 while (!feof($base)) 
 	{
@@ -25,6 +51,5 @@ else
 return $tel;
 fclose($base);
 }
-
-//FormatTelNum(74953587438)
+}
 ?>
