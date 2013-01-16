e6927acd4baf98693d93bb7b9914d14fc4d858e6
root <root@jde.dnsalias.net>
2013-01-16 14:52:47 +0400

    Добавлены локальные имена в журнал

--- dialstat.php
+++ dialstat.php
@@ -1,7 +1,7 @@
 <?php 
 include 'formatnum.php'; // FormatTelNum() Форматирование и геостатус номера
-
-
+include 'localname.php';
+echo LocalName('42');
 
 // Ищем в файле конфигурации FreePBX логин и пароль к базе
 $login=exec("grep AMPDBUSER /etc/amportal.conf|grep -v '^#'|tail -n 1|awk -F '=' '{print $2}'");
@@ -11,14 +11,15 @@ mysql_connect("127.0.0.1", $login, $password) or die(mysql_error());
 mysql_select_db("asterisk") or die(mysql_error());
 
 
-$strSQL = ("select extension from users");
+$strSQL = ("select * from users");
 mysql_query("SET lc_time_names = 'ru_RU'");
-$rs = mysql_query($strSQL);
+$rs_users = mysql_query($strSQL);
+
 
 //Для src "and (src='42' or src='41' or src='04')"
 $str_src="(";
 
-while($row = mysql_fetch_array($rs)) 
+while($row = mysql_fetch_array($rs_users)) 
  {$str_src=$str_src."src='".$row[0]."' or ";}
 
 $str_src=$str_src."src='0')";
@@ -83,14 +84,12 @@ $rs = mysql_query($strSQL);
 
 echo "<table border='1'>";
 echo "<tr><td>Дата звонка<td>Внутренний<td>Направление<td>Внешний<td>CLID<td>Разговор<td>Ожидание<td>ID</td>";
-
 while($row = mysql_fetch_array($rs))
 
 {
-
 echo "<tr>" .
                "<td>" . $row[0] . 
-               "<td>" . $row[2] .
+               "<td>" . LocalName($row[2]) .
                "<td>" . $row[7] .
                "<td>" . FormatTelNum($row[3]) .
                "<td>" . $row[1] .
