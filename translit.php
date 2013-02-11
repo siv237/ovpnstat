96de1ad554dcf348921e5284c598cf4ea7b71c0d
root <root@jdeoffice.local>
2013-02-11 11:42:00 +1100

    Исправлен ГОСТ на транслит. Теперь 7.79-2000

--- translit.php
+++ translit.php
@@ -1,16 +1,16 @@
 <?php
 function latrus ($string) # Теперь задаём функцию перекодировки транслита в кириллицу.
 {
-$string = ereg_replace("Ja","Я",$string);
-$string = ereg_replace("ju","ю",$string);
-$string = ereg_replace("jo","ё",$string);
-$string = ereg_replace("Jo","Ё",$string);
+$string = ereg_replace("Ya","Я",$string);
+$string = ereg_replace("yu","ю",$string);
+$string = ereg_replace("yo","ё",$string);
+$string = ereg_replace("Yo","Ё",$string);
 $string = ereg_replace("zh","ж",$string);
 $string = ereg_replace("Zh","Ж",$string);
-$string = ereg_replace("jj","й",$string);
-$string = ereg_replace("Jj","Й",$string);
-$string = ereg_replace("kh","х",$string);
-$string = ereg_replace("Kh","Х",$string);
+$string = ereg_replace("j","й",$string);
+$string = ereg_replace("J","Й",$string);
+$string = ereg_replace("x","х",$string);
+$string = ereg_replace("X","Х",$string);
 $string = ereg_replace("ch","ч",$string);
 $string = ereg_replace("Ch","Ч",$string);
 $string = ereg_replace("sh","ш",$string);
@@ -19,10 +19,10 @@ $string = ereg_replace("shh","щ",$string);
 $string = ereg_replace("Shh","Щ",$string);
 $string = ereg_replace("'","ь",$string);
 $string = ereg_replace("''","Ь",$string);
-$string = ereg_replace("eh","э",$string);
-$string = ereg_replace("Ee","Э",$string);
-$string = ereg_replace("Ju","Ю",$string);
-$string = ereg_replace("jа","я",$string);
+$string = ereg_replace("e'","э",$string);
+$string = ereg_replace("E'","Э",$string);
+$string = ereg_replace("Yu","Ю",$string);
+$string = ereg_replace("yа","я",$string);
 
 
 $string = ereg_replace("a","а",$string);
@@ -37,16 +37,16 @@ $string = ereg_replace("d","д",$string);
 $string = ereg_replace("D","Д",$string);
 $string = ereg_replace("e","е",$string);
 $string = ereg_replace("E","Е",$string);
-$string = ereg_replace("jo","ё",$string);
-$string = ereg_replace("Jo","Ё",$string);
+$string = ereg_replace("yo","ё",$string);
+$string = ereg_replace("Yo","Ё",$string);
 $string = ereg_replace("zh","ж",$string);
 $string = ereg_replace("Zh","Ж",$string);
 $string = ereg_replace("z","з",$string);
 $string = ereg_replace("Z","З",$string);
 $string = ereg_replace("i","и",$string);
 $string = ereg_replace("I","И",$string);
-$string = ereg_replace("jj","й",$string);
-$string = ereg_replace("Jj","Й",$string);
+$string = ereg_replace("j","й",$string);
+$string = ereg_replace("J","Й",$string);
 $string = ereg_replace("k","к",$string);
 $string = ereg_replace("K","К",$string);
 $string = ereg_replace("l","л",$string);
@@ -69,8 +69,8 @@ $string = ereg_replace("u","у",$string);
 $string = ereg_replace("U","У",$string);
 $string = ereg_replace("f","ф",$string);
 $string = ereg_replace("F","Ф",$string);
-$string = ereg_replace("kh","х",$string);
-$string = ereg_replace("Kh","Х",$string);
+$string = ereg_replace("x","х",$string);
+$string = ereg_replace("X","Х",$string);
 $string = ereg_replace("c","ц",$string);
 $string = ereg_replace("C","Ц",$string);
 $string = ereg_replace("ch","ч",$string);
@@ -86,9 +86,9 @@ $string = ereg_replace("Y","Ы",$string);
 $string = ereg_replace("'","ь",$string);
 $string = ereg_replace("''","Ь",$string);
 $string = ereg_replace("eh","э",$string);
-$string = ereg_replace("Ee","Э",$string);
-$string = ereg_replace("Ju","Ю",$string);
-$string = ereg_replace("jа","я",$string);
+$string = ereg_replace("E'","Э",$string);
+$string = ereg_replace("Yu","Ю",$string);
+$string = ereg_replace("Yа","я",$string);
 
 return $string;
 }
