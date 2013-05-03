14f3443e092ad3e57019657967680bb77da48be8
root <root@jdeoffice.local>
2013-05-03 15:52:37 +1100

    В журнал звонков добавлен поиск и транзитные звонки

--- dialstat.php
+++ dialstat.php
@@ -2,6 +2,42 @@
 include 'formatnum.php'; // FormatTelNum() Форматирование и геостатус номера
 include 'localname.php';
 
+// Считываем переданые параметры поиска и если их нет задаем дефолты
+$date_to=$_POST["date_to"];
+$date_from=$_POST["date_from"];
+$str_find=$_POST["str_find"];
+if ($_POST["str_limit"] == "" ) 
+	{$str_limit="100";}
+else	{$str_limit=$_POST["str_limit"];}
+
+if(!isset($date_from)){$date_from=date("m/01/Y");}
+if(!isset($date_to)){$date_to=date("m/d/Y");}
+
+// Рисуем форму для поиска
+//echo "<table border='0'><td><td><td>Поиск<td>Лимит<td></tr>";
+echo "<table border='0'>";
+echo "
+<form method='post' action=''>
+
+<! -- Добавляем скрипт календаря для удобства задания периода в форме поиска/--!>
+
+ <link rel='stylesheet' type='text/css' href='cal/tcal.css' />
+ <script type='text/javascript' src='cal/tcal.js'></script>
+
+<td>	Дата начала: <input type='text' name='date_from' class='tcal' value='".$date_from."' SIZE=8> 
+<td>	Дата окончания: <input type='text' name='date_to' class='tcal' value='".$date_to."' SIZE=8>
+<td>	Поиск: <input type='text' name='str_find' value='".$str_find."'>
+<td>	Лимит: <input type='text' name='str_limit' value='".$str_limit."' SIZE=4>
+<td>	<input type='submit' name='submit' value='Найти'>
+</td>
+</table>
+</form>
+<br>
+";
+$FindDate="and (calldate BETWEEN STR_TO_DATE('".$date_from." 00:00:00','%m/%d/%Y %H:%i:%s') AND STR_TO_DATE('".$date_to." 23:59:59','%m/%d/%Y %H:%i:%s'))";
+$FindStr="and concat(clid,'|',src,'|',dst,'|',uniqueid,'|',recordingfile) like '%".$str_find."%'";
+
+
 // Ищем в файле конфигурации FreePBX логин и пароль к базе
 $login=exec("grep AMPDBUSER /etc/amportal.conf|grep -v '^#'|tail -n 1|awk -F '=' '{print $2}'");
 $password=exec("grep AMPDBPASS /etc/amportal.conf|grep -v '^#'|tail -n 1|awk -F '=' '{print $2}'");
@@ -50,8 +86,10 @@ $strSQL =
 	where 	lastapp='Dial' 
 	and 	".$str_src." 
 	and not	".$str_dst." 
-	order 	by calldate 
-	desc 	limit 100
+	$FindDate
+	$FindStr
+	order 	by calldate desc 	
+	limit $str_limit
  )
 	union
  (
@@ -68,11 +106,37 @@ $strSQL =
         where   disposition='ANSWERED' 
         and     ".$str_dst." 
         and not ".$str_src." 
-        order   by calldate  desc    
-	limit 100
+        $FindDate
+	$FindStr
+	order   by calldate  desc    
+	limit $str_limit
  )
+
+        union
+ (
+        select  calldate,
+                clid,
+                src,
+                dst,
+                duration,
+                billsec,
+                uniqueid,
+                'T&#9658',
+                recordingfile 
+        from    cdr 
+        where   disposition='ANSWERED' 
+        and not ".$str_dst." 
+        and not ".$str_src." 
+	and lastapp='Dial'
+	$FindDate
+	$FindStr
+        order   by calldate  desc    
+        limit $str_limit
+ )
+
+
 order   by calldate desc
-limit 200
+limit $str_limit
 
 ");
 
